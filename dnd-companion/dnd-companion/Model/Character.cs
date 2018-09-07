55ff7ef79509038ff1f311254f224251f1e8d635
Kaitlynn Kruse <kaitlynn@jane.com>
2018-09-07 11:07:08 -0600

    added needed attributes

--- dnd-companion/dnd-companion/Model/Character.cs
+++ dnd-companion/dnd-companion/Model/Character.cs
@@ -34,6 +34,10 @@ namespace dnd_companion.Model
 
         public int MaxHP { get; set; }
 
+        public int Initiative { get; set; }
 
+        public int Speed { get; set; }
+
+        public string HitDice { get; set; }
     }
 }
