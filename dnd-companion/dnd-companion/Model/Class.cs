f6132284814465cc1559032311a05a3c474a428f
Jordan Wittwer <jordanwittwer@gmail.com>
2018-09-07 10:51:37 -0600

    Added Class attributes to class page.

--- dnd-companion/dnd-companion/Model/Class.cs
+++ dnd-companion/dnd-companion/Model/Class.cs
@@ -7,5 +7,20 @@ namespace dnd_companion.Model
         {
         }
 
+        public string[] ClassName { get; set; }
+
+        public string Description { get; set; }
+
+        public string HitDie { get; set; }
+
+        public string HitPoint { get; set; }
+
+        public string PrimaryAbility { get; set; }
+        
+        public string[] SavingThrows { get; set; }
+
+        public string Equipment { get; set; }//includes weapons, armor, clothes, and tools. 
+
+        public string[] Skills { get; set; }
     }
 }
--- dnd-companion/dnd-companion/Model/Race.cs
+++ dnd-companion/dnd-companion/Model/Race.cs
@@ -6,6 +6,8 @@ namespace dnd_companion.Model
         public Race()
         {
         }
+
+        public string[] RaceName { get; set; }
         public string AbilityScoreIncrease { get; set; }
 
         public int Age { get; set; }
